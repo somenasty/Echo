df07b3837e0ec48a58fea34ef0b6d03c261bd0aa
somenasty <likun_1992@126.com>
2020-09-28 00:09:29 +0800

    Add files via upload
    add code to determine if CSV file has header

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-from StcokInfo import *
-from InsertInfo import *
-from DeleteInfo import *
+import csv
 import os
+
+from DeleteInfo import *
+from InsertInfo import *
+from StcokInfo import *
 
 # s = StockInfo("加多宝", "1000", "1000")
 # s.debug_print()
@@ -13,16 +15,19 @@ import os
 # # i.add_headers(headers, csv_file_name)
 # i.insert_to_csv(single_stock, csv_file_name)
 # print(os.getcwd())
-headers_flag = 0
 flag = 0
 csv_file_name = "test.csv"
 headers = ["日期", "股票名称", "股票价格", "股票数量", "股票总价"]
+headers_flag = True
 
 while flag != 1:
-    if headers_flag == 0:
-        insert_headers = InsertInfo()
-        insert_headers.add_headers(headers, csv_file_name)
-        headers_flag += 1
+    with open(csv_file_name, "r", newline="")as f1:
+        read_csv = csv.reader(f1)
+        headers_flag = [x for x in read_csv]
+        if len(headers_flag) == 0:
+            headers_flag = False
+            insert_headers = InsertInfo()
+            insert_headers.add_headers(headers, csv_file_name)
     get_choice = input("1.录入 2.删除 3.退出\n")
     if get_choice == '1':
         get_input = input("股票名称 股票价格 股票数量 (买入0/卖出1)\n")
